b57be0e3da91ad8ec6e6306746131b8ab2d3760d
satooooshi <satoaikawa@qq.com>
2021-02-25 12:55:00 +0900

    aa

---
+++ qualificationsimulator.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i, n) for (ll i=0; i < n; i++)  // 0 ~ n-1
+
+int main(){
+
+    int n,a,b;
+    cin>>n>>a>>b;
+
+    string s;//si:a,b // top ith domestic/abroad competitant 
+    cin>>s;
+    
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='a'){
+            
+        }else{
+
+        }
+    }
+
+
+    return 0;
+}
