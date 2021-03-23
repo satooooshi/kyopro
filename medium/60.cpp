a597767c5f72f9872f10b46ffcafacc28acb8a2d
satoshi aikawa <satoshiaikawa@satoshinoMacBook-Pro.local>
2021-03-23 16:37:10 +0900

    march string formation bug hasnt fixed.

---
+++ medium/60.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i=0; i < n; i++)  // 0 ~ n-1
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    map<char,int>m;
+    rep(i,n){
+        string s;
+        cin>>s;
+        m[s[0]]++;
+    }
+    
+    // 5C3 == 10 combis
+    vector<string>strs={"mar","mac","mah","mrc","mrh","arc","arh",
+                    "rch","mch","ach"};
+
+    //for(auto a:m)cout<<a.first<<" "<<a.second<<endl;
+    ll cnt=0;
+    for(string t:strs){
+        cnt+=m[t[0]-32]*m[t[1]-32]*m[t[2]-32];
+    }
+    cout<<cnt<<endl;
+
+
+    return 0;
+}
