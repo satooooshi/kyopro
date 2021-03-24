b6996d004e6724c9a13cf75ef6f9625517e2e68d
satoshi aikawa <satoshiaikawa@satoshinoMacBook-Pro.local>
2021-03-24 16:46:00 +0900

    60.cpp march, m[] returns int git add . has to convert to long long with 1LLgit add .

--- medium/60.cpp
+++ medium/60.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef long long ll; // 64-bit long
 #define rep(i, n) for (ll i=0; i < n; i++)  // 0 ~ n-1
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
@@ -10,6 +10,7 @@ int main(){
 
     int n;
     cin>>n;
+    string march="MARCH";
     map<char,int>m;
     rep(i,n){
         string s;
@@ -17,14 +18,17 @@ int main(){
         m[s[0]]++;
     }
     
-    // 5C3 == 10 combis
-    vector<string>strs={"mar","mac","mah","mrc","mrh","arc","arh",
-                    "rch","mch","ach"};
+    
 
     //for(auto a:m)cout<<a.first<<" "<<a.second<<endl;
     ll cnt=0;
-    for(string t:strs){
-        cnt+=m[t[0]-32]*m[t[1]-32]*m[t[2]-32];
+    // 5C3
+    for(int i=0;i<march.length();i++){
+        for(int j=i+1;j<march.length();j++){
+            for(int k=j+1;k<march.length();k++){
+                cnt+=1LL*m[march[i]]*m[march[j]]*m[march[k]];// m[] returns int !! has to convert to long long with 1LL!!
+            }
+        }
     }
     cout<<cnt<<endl;
 
