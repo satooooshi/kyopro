b276706ec6ffb6fe6620d1b3d9cd7f92e2e7641b
satoshi aikawa <satoshiaikawa@satoshinoMacBook-Pro.local>
2021-03-24 21:49:03 +0900

    65.cpp A - Two Abbreviations gl==nm

---
+++ medium/65.cpp
@@ -0,0 +1,39 @@
+// A - Two Abbreviations
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i=0; i < n; i++)  // 0 ~ n-1
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+// gcd, lcm
+// a と b の最大公約数を返す関数
+long long gcd(long long a, long long b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+
+long long lcm(long long a, long long b) {
+    return a / gcd(a, b) * b;
+}
+
+int main(){
+    ll n,m;
+    cin>>n>>m;
+    string s,t;
+    cin>>s>>t;
+
+    // gl==nm
+    ll g=gcd(n,m);
+    ll l=lcm(n,m);
+    bool ok=true;
+    for(ll i=0;i<g;i++){
+        if(s[i*n/g]!=t[i*m/g])ok=false;
+    }
+    if(ok)cout<<l<<endl;
+    else cout<<-1<<endl;
+    
+
+
+    return 0;
+}
