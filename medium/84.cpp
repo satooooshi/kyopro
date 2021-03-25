31126ac14bfc7f70942916ea6020672e8c5ab027
satooooshi <satoaikawa@qq.com>
2021-03-25 13:03:28 +0900

    84.cpp triangle using binary search(lower_bound(a+b)) is also ok

---
+++ medium/84.cpp
@@ -0,0 +1,35 @@
+// D - Triangles
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i=0; i < n; i++)  // 0 ~ n-1
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+
+int main(){
+
+    ll n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+    sort(v.begin(),v.end());
+
+    // a<=b<=c && a+b>=c 
+    ll cnt=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                if(v[i]+v[j]>v[k])cnt++;
+            }
+        }
+    }
+    cout<<cnt<<endl;
+
+
+    return 0;
+}
