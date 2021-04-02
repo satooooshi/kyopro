ef306a4c0c722a96a5799a0404945ef9c4e3b9ed
satooooshi <satoaikawa@qq.com>
2021-04-02 16:00:16 +0900

    kenken race

--- medium/58.cpp
+++ medium/58.cpp
@@ -9,16 +9,18 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 
 int main(){
 
-    int n,a,b,c,d;
+    ll n,a,b,c,d;
     string s;
     cin>>n>>a>>b>>c>>d>>s;
+    a--;
+    b--;
+    c--;
+    d--;
 
     if(c==d){
         cout<<"No"<<endl;
         return 0;
-    }
-
-    if(c<d){ // move b to the d first
+    }else if(c<d){ // move b to the d first, (b,d) (a,c) do not have ##
         for(int i=a+1;i<c;i++){
             if(s[i-1]=='#'&&s[i]=='#'){
                 cout<<"No"<<endl;
@@ -34,7 +36,19 @@ int main(){
         cout<<"Yes"<<endl;
         return 0;
     }else{ // [b-1,d+1] has ... for a to jump over b in this section
-        for(int i=b+1;i<d+1;i++){
+        for(int i=a+1;i<c;i++){
+            if(s[i-1]=='#'&&s[i]=='#'){
+                cout<<"No"<<endl;
+                return 0;
+            }
+        }
+        for(int i=b+1;i<d;i++){
+            if(s[i-1]=='#'&&s[i]=='#'){
+                cout<<"No"<<endl;
+                return 0;
+            }
+        }
+        for(int i=b+1;i<=d+1;i++){
             if(s[i-2]=='.'&&s[i-1]=='.'&&s[i]=='.'){
                 cout<<"Yes"<<endl;
                 return 0;
