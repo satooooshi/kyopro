a9b24e8e430dbd9210eda10be71bc88838b8412e
satoshi aikawa <satoshiaikawa@satoshinoMacBook-Pro.local>
2021-10-18 21:35:05 +0800

    green

--- green/abc172_d.cpp
+++ green/abc172_d.cpp
@@ -1,23 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i,a,b) for(int i=a;i<b;i++)
-#define rrep(i,a,b) for(int i=a;i>=b;i--)
-#define fore(i,a) for(auto &i:a)
-#define all(x) (x).begin(),(x).end()
 
-//void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
-typedef long long ll; 
-const int inf = INT_MAX / 2; 
-const ll infl = 1LL << 60;
-template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
-
-
-int main(){
-
+int main() {
     int n;cin>>n;
+    vector<int>f(n);
+    for(int k=1;k<=n;k++){
+        f[k]=n/k;
+    }
+    // eratos の表、横軸divs,縦軸k,調和級数的計算量 O(Nlog2N)
+    long long res=0;
+    for(int a=1;a<=n;a++){ // k's div
+        for(int b=a;b<=n;b+=a){ 
+            res+=b;
+        }
+    }
+    cout<<res<<endl;
 
-    
     return 0;
 }
